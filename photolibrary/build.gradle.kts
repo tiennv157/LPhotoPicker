3a59a9ec9d898af51470dee5a6466d25adfd20cd
tiennv157 <tiennv157@gmail.com>
2026-06-02 15:30:13 +0700

    update dependence

--- photolibrary/build.gradle.kts
+++ photolibrary/build.gradle.kts
@@ -4,7 +4,6 @@ import java.util.Properties
 
 plugins {
     alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
 
     `maven-publish`
     signing
@@ -66,90 +65,3 @@ dependencies {
 //---------- maven upload info -----------------------------------
 
 val versionName = "3.2"
-
-var signingKeyId = ""//签名的密钥后8位
-var signingPassword = ""//签名设置的密码
-var secretKeyRingFile = ""//生成的secring.gpg文件目录
-
-
-val localProperties: File = project.rootProject.file("local.properties")
-
-if (localProperties.exists()) {
-    println("Found secret props file, loading props")
-    val properties = Properties()
-
-    InputStreamReader(FileInputStream(localProperties), Charsets.UTF_8).use { reader ->
-        properties.load(reader)
-    }
-    signingKeyId = properties.getProperty("signing.keyId")
-    signingPassword = properties.getProperty("signing.password")
-    secretKeyRingFile = properties.getProperty("signing.secretKeyRingFile")
-
-} else {
-    println("No props file, loading env vars")
-}
-
-afterEvaluate {
-
-    publishing {
-        publications {
-            create<MavenPublication>("release") {
-                from(components.findByName("release"))
-                groupId = "io.github.limuyang2"
-                artifactId = "LPhotoPicker"
-                version = versionName
-
-                pom {
-                    name.value("msnet")
-                    description.value("A network request library.")
-                    url.value("https://github.com/limuyang2/LPhotoPicker")
-
-                    licenses {
-                        license {
-                            //协议类型
-                            name.value("The MIT License")
-                            url.value("https://github.com/limuyang2/LPhotoPicker/blob/main/LICENSE")
-                        }
-                    }
-
-                    developers {
-                        developer {
-                            id.value("limuyang2")
-                            name.value("limuyang")
-                            email.value("limuyang2@hotmail.com")
-                        }
-                    }
-
-                    scm {
-                        connection.value("scm:git@github.com:limuyang2/RealtimeBlurView.git")
-                        developerConnection.value("scm:git@github.com:limuyang2/RealtimeBlurView.git")
-                        url.value("https://github.com/limuyang2/LPhotoPicker")
-                    }
-                }
-            }
-
-        }
-
-        repositories {
-            maven {
-                setUrl("$rootDir/RepoDir")
-            }
-        }
-    }
-
-}
-
-gradle.taskGraph.whenReady {
-    if (allTasks.any { it is Sign }) {
-
-        allprojects {
-            extra["signing.keyId"] = signingKeyId
-            extra["signing.secretKeyRingFile"] = secretKeyRingFile
-            extra["signing.password"] = signingPassword
-        }
-    }
-}
-
-signing {
-    sign(publishing.publications)
-}
